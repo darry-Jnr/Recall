297726d6915c1c5e7d1e5af6fc8cfec00c26a079
darry-Jnr <oluwadarasotomi@gmail.com>
2026-07-28 22:17:49 +0100

    fix: allow landing page to scroll on desktop

--- web/src/components/landing/Landing.tsx
+++ web/src/components/landing/Landing.tsx
@@ -4,7 +4,7 @@ import { Footer } from "./Footer";
 
 export function Landing() {
   return (
-    <div className="flex flex-col h-screen relative overflow-hidden">
+    <div className="flex flex-col min-h-screen relative">
       <div className="absolute top-1/3 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[800px] h-[500px] bg-accent/[0.03] rounded-full blur-[120px] pointer-events-none" />
       <Navbar />
       <Hero />
